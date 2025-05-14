03df28cdd05f228819f0cba3240ee2d0a0b2cc9b
DattiVishnuVandana <vandanadatti567@gmail.com>
2025-05-14 17:37:10 +0530

    setTask,clearAll

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
 
-export const AddTask = () => {
+
+export const AddTask = ({tasklist,setTasklist}) => {
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        const date=new Date();
+        const newTask={
+            id:date.getTime(),
+            name:e.target.task.value,
+            time:`${date.toLocaleTimeString()} ${date.toLocaleDateString()}`
+        }
+        setTasklist([...tasklist,newTask])
+        e.target.task.value=""
+    }
   return (
     <div className='middle'>
-        <form action="">
+        <form onSubmit={handleSubmit}>
             <input type="text"  name="task" placeholder='enter u r task' maxLength={25} autoComplete='on'/>
             <button type="submit">Submit</button>
         </form>
--- src/components/ShowTask.jsx
+++ src/components/ShowTask.jsx
@@ -1,30 +1,27 @@
 import React from 'react'
 
-export const ShowTask = () => {
-    const TaskList=[{id:10001,name:"Task1",time:"90:09 AM "},
-        {id:10002,name:"Task2",time:"10:09 AM "},
-        {id:10003,name:"Task3",time:"11:09 AM "}
-    ]
+export const ShowTask = ({tasklist,setTasklist}) => {
+   
   return (
    <div className="showTask">
 <div className="head">
     <div className='inside'>
           <span className="Todo">Todo</span>
-    <span className="count">0</span>
+    <span className="count">{tasklist.length}</span>
     </div>
-  <button className="clearAll">clear All</button>
+  <button className="clearAll" onClick={()=>setTasklist([])}>clear All</button>
 </div>
 <ul>
-    {TaskList.map(task=>(
-        <li>
+    {tasklist.map(task=>(
+        <li key={task.id}>
             <p>
                 
                 <span className="name">{task.name}</span>
                 <span className="time">{task.time}</span>
             </p>
             <div className="icons">
-                 <i class="fa-solid fa-pen-to-square i1"></i>
-            <i class="fa-solid fa-trash i2"></i>
+                 <i className="fa-solid fa-pen-to-square i1"></i>
+            <i className="fa-solid fa-trash i2"></i>
             </div>
            
         </li>
