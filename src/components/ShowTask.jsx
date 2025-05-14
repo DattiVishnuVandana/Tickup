59a0acff169bc883265c6f97c3558852f9f3d720
DattiVishnuVandana <vandanadatti567@gmail.com>
2025-05-14 17:19:23 +0530

    "components2"

--- src/components/ShowTask.jsx
+++ src/components/ShowTask.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
 
 export const ShowTask = () => {
+    const TaskList=[{id:10001,name:"Task1",time:"90:09 AM "},
+        {id:10002,name:"Task2",time:"10:09 AM "},
+        {id:10003,name:"Task3",time:"11:09 AM "}
+    ]
   return (
-   <div className="showtask">
+   <div className="showTask">
+<div className="head">
+    <div className='inside'>
+          <span className="Todo">Todo</span>
+    <span className="count">0</span>
+    </div>
+  <button className="clearAll">clear All</button>
+</div>
+<ul>
+    {TaskList.map(task=>(
+        <li>
+            <p>
+                
+                <span className="name">{task.name}</span>
+                <span className="time">{task.time}</span>
+            </p>
+            <div className="icons">
+                 <i class="fa-solid fa-pen-to-square i1"></i>
+            <i class="fa-solid fa-trash i2"></i>
+            </div>
+           
+        </li>
+    ))}
+</ul>
 
-    
+
    </div>
   )
 }
